fd3489da5ce8026c673f29253a574d16def80f71
Jason Shaw <yourhoward@gmail.com>
2020-11-10 22:32:23 -0800

    added array prototype methods

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,31 @@
-import { greet } from './'
+import { loop } from './'
 
-describe('greet function test', () => {
-	test('returns a string', () => {
-		const str = greet()
-		expect(typeof str).toEqual('string')
+describe('loop tests', () => {
+	test('loops around end of array', () => {
+		const arr = [1, 2, 3]
+		const looper = loop(arr)
+
+		expect(looper.next().value).toBe(1)
+		expect(looper.next().value).toBe(2)
+		expect(looper.next().value).toBe(3)
+		expect(looper.next().value).toBe(1)
+	})
+})
+
+describe('Array prototype tests', () => {
+	test('#last returns value at last index', () => {
+		const arr = [1, 2, 3]
+
+		expect(arr.last()).toBe(3)
+		arr[2] = 100
+		expect(arr.last()).toBe(100)
+	})
+
+	test('#first returns value at first index', () => {
+		const arr = [1, 2, 3]
+
+		expect(arr.first()).toBe(1)
+		arr[0] = 100
+		expect(arr.first()).toBe(100)
 	})
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-export const greet = (): string => 'Hello, World!'
+export function* loop(arr: any[]) {
+    for (let i = 0; ; i++) {
+        if (i === arr.length) i = 0
+        yield arr[i]
+    }
+}
+
+if (!Array.prototype.hasOwnProperty('last')) {
+    Array.prototype.last = function () {
+        return this[this.length - 1]
+    }
+}
+
+if (!Array.prototype.hasOwnProperty('first')) {
+    Array.prototype.first = function () {
+        return this[0]
+    }
+}
